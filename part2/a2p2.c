dcc7eb9cfa902147e9795fc9fcc07c47e42804a9
matt <mjbraun@ualberta.ca>
2023-03-04 19:57:46 -0700

    Submission

--- part2/a2p2.c
+++ part2/a2p2.c
@@ -1,50 +1,53 @@
 #include <assert.h> 
 #include <assert.h> 
+#include <errno.h>
 #include <fcntl.h>
 #include <poll.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
 
-#define NCLIENT 3
 #define BUFSIZE 80
-#define NUM_PACKET_TYPES 7
-#define MAX_PACKET_TYPE_LEN 6
+#define MAXCMD 10
+#define MAXPATH 50
 #define MAXWORD 32
+#define MAX_PACKET_TYPE_LEN 6
+#define NCLIENT 3
+#define NUM_PACKET_TYPES 7
 
-// TODO: Create a packet struct/object
+typedef enum { PUT, GET, DELETE, GTIME, TIME, OK, ERROR } Packet_Type;
 
-// Source: eclass fifoMsg.c
-typedef enum { PUT, GET, DELETE, GTIME, TIME, OK, ERROR } Packet_Type;	  // Message packet_types
-
-typedef struct { char message[BUFSIZE]; } Message;
+typedef struct { char message[MAXWORD]; } Message;
 typedef struct { Packet_Type packet_type; Message message; } Packet;
 
+char* process_message(Message received_message) {
+   char* file_name = malloc(sizeof(char) * MAXWORD);
+   strcpy(file_name, received_message.message);
+   return file_name;
+}
 
-
-//####
+//Source: fifoMsg.c
 void send_packet (int fd, Packet_Type packet_type, Message *message)
 {
     Packet  packet;
-
     assert (fd >= 0);
     memset( (char *) &packet, 0, sizeof(packet) );
     packet.packet_type= packet_type;
     packet.message=  *message;
     write (fd, (char *) &packet, sizeof(packet));
 }
-//####
+
+//Source: fifoMsg.c
 Packet receive_packet (int fd)
 {
     int    len; 
     Packet  packet;
-
     assert (fd >= 0);
     memset( (char *) &packet, 0, sizeof(packet) );
     len= read (fd, (char *) &packet, sizeof(packet));
@@ -77,15 +80,13 @@ void spawn_client(int *fifo_cs_fd, int *fifo_sc_fd, char* client_ID, char* input
         exit(EXIT_FAILURE);
     }
 
-    // TODO: Test after send packets
-    // // Initialize pollfd array
-    // // Initialize pollfd array to receive from all potential clients
-    // int client_rcv_fds[1] = {fifo_sc_fd};
-    // struct pollfd client_rcv_pfds[1];
-    // client_rcv_pfds[0].fd = client_rcv_fds[0];
-    // client_rcv_pfds[0].events = POLLIN;
-    // client_rcv_pfds[0].revents = 0;
-    // int timeout = -1; //TODO?: Change to 0 or 1
+    // Initialize pollfd array
+    int client_rcv_fds[1] = {fifo_sc_fd};
+    struct pollfd client_rcv_pfds[1];
+    client_rcv_pfds[0].fd = client_rcv_fds[0];
+    client_rcv_pfds[0].events = POLLIN;
+    client_rcv_pfds[0].revents = 0;
+    int timeout = -1; //TODO?: Change to 0 or 1
 
     // Client loop
     // Read input_file line-by-line
@@ -133,37 +134,30 @@ void spawn_client(int *fifo_cs_fd, int *fifo_sc_fd, char* client_ID, char* input
 
         // Assign message
         if (packet_type != PUT && packet_type != GET && packet_type != DELETE && packet_type != GTIME) continue; // No packets sent for delay case
-        if (packet_type == PUT || packet_type == GET || packet_type == DELETE) strcpy(message.message, arg3); // Load object into message
+        if (packet_type == PUT || packet_type == GET || packet_type == DELETE) {
+            // Load object into message
+            strcpy(message.message, arg3);
+        } 
 
         // Send packet
-        // printf("debug: Attempting to send a [%s] packet from client [%s]...\n", arg2, arg1);
         send_packet(*fifo_cs_fd, packet_type, &message);
-        printf("Transmitted (src= client:%s) %s", client_ID, packet_type);
+        printf("Transmitted (src= client:%s) %s\n", client_ID, packet_type);
 
-        // int num_events = poll(client_rcv_fds, 1, timeout);
-        // // Check that poll worked properly
-        // if (num_events == -1) {
-        //     perror("Error: Error polling on the client side.\n");
-        //     exit(EXIT_FAILURE);
-        // }
-        // // Check for event from server
-        // if (client_rcv_pfds[0].revents & POLLIN) {
-        //     printf("client %d received event from server", client_ID);
-        // }
+        // TODO: Wait for server reply
+        while (1) {
+            int num_events = poll(client_rcv_fds, 1, timeout);
+            // Check that poll worked properly
+            if (num_events == -1) {
+                perror("Error: Error polling on the client side.\n");
+                exit(EXIT_FAILURE);
+            }
+            // Check for event from server
+            if (client_rcv_pfds[0].revents & POLLIN) {
+                printf("client %d received event from server\n", client_ID);
+                break;
+            }
+        }
 
-        // Write packet buffer to server
-        // printf("Attempting to send packet from client... \n[%s]\n", packet_buffer);
-        // ssize_t nread = strlen(packet_buffer);
-        // ssize_t buffer_len = write(fifo_cs_fd, packet_buffer, nread);
-        // if (buffer_len < 0 && errno != EAGAIN) {
-        //     perror("Error: FIFO writing error\n");
-        //     exit(EXIT_FAILURE);
-        // }
-
-        // Check if success
-        // if (buffer_len > 0) {
-        //     printf("Send succesful! \n[%s]\n", packet_buffer);
-        // }
 
     }
     // Close fp and FIFOs
@@ -176,6 +170,7 @@ void spawn_client(int *fifo_cs_fd, int *fifo_sc_fd, char* client_ID, char* input
 
 //Source: eClass
 void launch_server(int *fifo_c1_s_fd, int *fifo_c2_s_fd, int *fifo_c3_s_fd, int *fifo_s_c1_fd, int *fifo_s_c2_fd, int *fifo_s_c3_fd) {
+    clock_t start_time = clock();
     char packet_buffer[BUFSIZE];
     // Open all 6 FIFOs
     // To server
@@ -230,59 +225,57 @@ void launch_server(int *fifo_c1_s_fd, int *fifo_c2_s_fd, int *fifo_c3_s_fd, int 
                 received_packet = receive_packet(active_cs_fifo);
                 Packet_Type received_type = received_packet.packet_type;
                 Message received_message = received_packet.message;
-                printf("  debug: Packet type: %d", received_type);
-                printf("  debug: Message: %s", received_message.message);
-                if (received_packet.packet_type == PUT) {
-                    
-                } else if (received_packet.packet_type == GET) {
-                    
-                } else if (received_packet.packet_type == DELETE) {
-                    
-                } else if (received_packet.packet_type == GTIME) {
-                    
+                printf("  debug: Packet type: %s\n", received_type);
+                printf("  debug: Message: %s\n", received_message.message);
+                char target_path_server[MAXPATH] = "server_directory/";
+                char target_path_client[MAXPATH] = "client_directory/";
+                
+                if (received_type == PUT) {
+                    // PUT: Client upload
+                    char *file_name = process_message(received_message);
+                    strcat(target_path_server, file_name);
+                    // If the object already exists in the server directory, throw an error
+                    if (access(target_path_server, F_OK) != -1) {
+                        char reply[MAXWORD] = "ERROR: object already exists";
+                    }
+                    // If not, make it exist
+                    else {
+                        char command[MAXCMD + MAXWORD];
+                        sprintf(command, "touch %s", target_path_server);
+                        system(command);
+                    }
+                    printf("Transmitted (src= client:%s) (%s: %s)\n", client_id, received_type, received_message.message);
+                } else if (received_type == GET) {
+                    // GET: Client download
+                    char *file_name = process_message(received_message);
+                    strcat(target_path_server, file_name);
+                    // If the object doesn't exist in the server directory, throw an error
+                    if (access(target_path_server, F_OK) == -1) {
+                        char reply[MAXWORD] = "ERROR: object not found\n";
+                    }
+                    // If it DOES exist in the server, make it exist in the client
+                    else {
+                        char command[MAXCMD + MAXWORD];
+                        sprintf(command, "touch %s", target_path_client);
+                        system(command);
+                    }
+                    printf("Received (src= server:%s) (%s: %s)\n", client_id, received_type, received_message.message);
+                } else if (received_type == DELETE) {
+                    printf("Transmitted (src= client:%s) (%s: %s)\n", client_id, received_type, received_message.message);
+                } else if (received_type == GTIME) {
+                    clock_t end_time = clock();
+                    double time_since_server_start = (((double) (end_time - start_time)) / CLOCKS_PER_SEC * 1000.0);
+                    printf("Transmitted (src= server) (TIME: %d)\n", time_since_server_start);
+                } else {
+                    perror("Error: Invalid received packet type (index)\n");    
                 }
-                printf("Transmitted (src= client:%s) %s", client_ID, packet_type);
-                    case PUT:
-                        //TODO: execute put object
-                    case GET:
-                        //TODO: execute get object
-                    case DELETE:
-                        //TODO: execute delete object
-                    case GTIME:
-                        //TODO: execute get time
-                    default:
-                        //Invalid index
-                        perror("Error: Invalid packet type (index)\n");    
-                }
-
-                // Read client file descriptor into buffer
-				// int buffer_len = read(fifo_fds[client_id], packet_buffer, BUFSIZE);
-                // printf ("debug: client= %d, buffer_len= %d\n", client_id, buffer_len);
-                // TODO: Identify command
-
-                // TODO: Execute command
                 // TODO: Reply with OK or ERROR w/description
-
-                // TODO?: Remove later, try testing it first
-                // Print 1 char at a time until end of buffer
-                // for (int i = 0; i < buffer_len; i++) 
-				// {
-				// 	if (packet_buffer[i] == -1)
-				// 	{ 
-                //         //done (message received?)
-                //         break;
-				// 	}
-				// 	else {
-                //         printf("[%c] ", packet_buffer[i]);
-                //     }
-				// }
 			}	
 		}
     }
-    
 }
 
-// Source: eClass, fifoMsg.c
+// Source: eClass: fifoMsg.c
 void FATAL (const char *fmt, ... )
 {
     va_list  ap;
@@ -294,35 +287,27 @@ void FATAL (const char *fmt, ... )
 
 
 
-//Source: eClass
+//Source: eClass: fifoMsg.c
 int main (int argc, char *argv[])
 {
-    // Spawn FIFOs
-    // int return_value = system("sh spawn_fifos.sh");
-    // if (return_value != 0) {
-    //     printf("FIFO creation failed.\n");
-    //     exit(0);
-    // }
-
     int fifo_c1_s_fd, fifo_c2_s_fd, fifo_c3_s_fd, fifo_s_c1_fd, fifo_s_c2_fd, fifo_s_c3_fd;
     
-
-
-    //Check if FIFOs are being opened properly?
-    if ( (fifo_c1_s_fd= open("fifo-1-0", O_RDWR)) < 0)
+    // Open FIFOs
+    if ( (fifo_c1_s_fd = open("fifo-1-0", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_c1_s_fd\n");
-    if ( (fifo_c2_s_fd= open("fifo-2-0", O_RDWR)) < 0)
+    if ( (fifo_c2_s_fd = open("fifo-2-0", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_c2_s_fd\n");
-    if ( (fifo_c3_s_fd= open("fifo-3-0", O_RDWR)) < 0)
+    if ( (fifo_c3_s_fd = open("fifo-3-0", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_c3_s_fd\n");
-    if ( (fifo_s_c1_fd= open("fifo-0-1", O_RDWR)) < 0)
+    if ( (fifo_s_c1_fd = open("fifo-0-1", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_s_c1_fd\n");
-    if ( (fifo_s_c2_fd= open("fifo-0-2", O_RDWR)) < 0)
+    if ( (fifo_s_c2_fd = open("fifo-0-2", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_s_c2_fd\n");
-    if ( (fifo_s_c3_fd= open("fifo-0-3", O_RDWR)) < 0)
+    if ( (fifo_s_c3_fd = open("fifo-0-3", O_RDWR)) < 0)
         FATAL ("%s: open '%s' failed ", argv[0], "fifo_s_c3_fd\n");
     
-    //Client check
+    // Check args
+    // Client check
     if ( strcmp(argv[1], "-c") == 0) {
         //Arg count check
         if (argc != 4) 
@@ -346,7 +331,6 @@ int main (int argc, char *argv[])
         }
 
     }
-        
     //Server check
     if ( strcmp(argv[1], "-s") == 0) {
         //Arg count check
@@ -358,6 +342,7 @@ int main (int argc, char *argv[])
         launch_server(&fifo_c1_s_fd, &fifo_c2_s_fd, &fifo_c3_s_fd, &fifo_s_c1_fd, &fifo_s_c2_fd, &fifo_s_c3_fd);
     } 
 
+    // Close FIFOs
     close(fifo_c1_s_fd); 
     close(fifo_c2_s_fd);
     close(fifo_c3_s_fd);
